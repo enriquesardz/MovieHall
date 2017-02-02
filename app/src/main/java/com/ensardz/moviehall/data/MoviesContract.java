ac072481576ccf7cccb05254cb89058a6db47bb6
Enrique Salas Rodríguez <Enrique Salas Rodrguez>
2017-02-02 14:00:27 -0600

    Modify MoviesContract database values definition Modify MoviesDBHelper database create statements are using the defined values from MoviesContract

--- app/src/main/java/com/ensardz/moviehall/data/MoviesContract.java
+++ app/src/main/java/com/ensardz/moviehall/data/MoviesContract.java
@@ -13,18 +13,25 @@ public class MoviesContract {
     public static final String CONTENT_AUTHORITY = "com.ensardz.android.moviehall.app";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
+    public static class VideosEntry implements BaseColumns{
+        public static final String TABLE_NAME = "videos";
+
+        public static final String COLUMN_NAME_KEY = "name";
+        public static final String COLUMN_YOUTUBE_KEY = "youtube_key";
+    }
+
     public static class MoviesEntry implements BaseColumns{
 
         public static final String TABLE_NAME = "movies";
 
-        public static final String _ID = "_id";
-        public static final String COLUMN_NAME_ID = "movie_id";
+        public static final String COLUMN_NAME_MOVIE_ID = "movie_id";
+        public static final String COLUMN_NAME_VIDEO_KEY = "videos_id";
         public static final String COLUMN_NAME_TITLE = "title";
         public static final String COLUMN_NAME_POSTER = "poster_path";
         public static final String COLUMN_NAME_OVERVIEW = "overview";
         public static final String COLUMN_NAME_VOTE_AVERAGE = "vote_average";
         public static final String COLUMN_NAME_RELEASE_DATE = "release_date";
         public static final String COLUMN_NAME_RUNTIME = "runtime";
-
+        public static final String COLUMN_NAME_IS_FAVOURITE = "is_favourite";
     }
 }
--- app/src/main/java/com/ensardz/moviehall/data/MoviesDBHelper.java
+++ app/src/main/java/com/ensardz/moviehall/data/MoviesDBHelper.java
@@ -1,7 +1,14 @@
 package com.ensardz.moviehall.data;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.media.MediaRecorder;
+
+import com.ensardz.moviehall.data.MoviesContract.VideosEntry;
+import com.ensardz.moviehall.data.MoviesContract.MoviesEntry;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 /**
  * Created by Quique on 01/02/2017.
@@ -10,11 +17,34 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class MoviesDBHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "movies.db";
+    public static final String DATABASE_NAME = "moviehall.db";
+
+    public MoviesDBHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_VIDEOS_TABLE = "CREATE TABLE " + VideosEntry.TABLE_NAME + " (" +
+                VideosEntry._ID + " INTEGER PRIMARY KEY, " +
+                VideosEntry.COLUMN_NAME_KEY + " TEXT NOT NULL, " +
+                VideosEntry.COLUMN_YOUTUBE_KEY + " TEXT NOT NULL " +
+                ");";
 
+        final String SQL_CREATE_MOVIES_TABLE = "CREATE TABLE " + MoviesEntry.TABLE_NAME + " (" +
+                MoviesEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                MoviesEntry.COLUMN_NAME_VIDEO_KEY + "INTEGER NOT NULL, " +
+                MoviesEntry.COLUMN_NAME_MOVIE_ID + "INTEGER NOT NULL, " +
+                MoviesEntry.COLUMN_NAME_TITLE + " TEXT NOT NULL, " +
+                MoviesEntry.COLUMN_NAME_POSTER + " TEXT NULL, " +
+                MoviesEntry.COLUMN_NAME_OVERVIEW + " TEXT NULL, " +
+                MoviesEntry.COLUMN_NAME_VOTE_AVERAGE + " REAL NULL, " +
+                MoviesEntry.COLUMN_NAME_RELEASE_DATE + " TEXT NULL, " +
+                MoviesEntry.COLUMN_NAME_RUNTIME + " INTEGER NULL, " +
+                MoviesEntry.COLUMN_NAME_IS_FAVOURITE + " INTEGER DEFAULT 0" +
+                ");";
+        db.execSQL(SQL_CREATE_VIDEOS_TABLE);
+        db.execSQL(SQL_CREATE_MOVIES_TABLE);
     }
 
     @Override
